9f7d5f12bc95aa93e30e27f2bb149de3763ce8b3
susan.novak@yahoo.com <susan.novak@yahoo.com>
2021-10-28 20:30:48 -0500

    Initial commit

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TechJobsPersistent.Controllers
 {
-    public class EmployerController : Controller
+    public class 
+        EmployerController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
